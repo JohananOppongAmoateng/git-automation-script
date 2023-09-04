8d1fab20944005c7db08147a1fce0befefa01c4d
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2023-09-04 01:57:57 +0000

    added script

---
+++ script.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+usage () { 
+    echo "$PROGNAME: usage: $PROGNAME [-f files | -i]" 
+    return 
+} 
+# Initialize variables to store the arguments
+# commit message
+echo "Please enter commit mesaage"
+read commit_message
+echo "Please enter filenames you want to add or . to add all files in current directory"
+read files
+
+function git_add_commit(){
+    current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+    if [ "$files" = "." ]; then
+    git add .
+    else
+        git add  "$files"
+    fi
+    echo "files added"
+    git commit -m"$commit_message"
+
+    }
+
+
+git_add_commit
+
+
+
+    
+
+
